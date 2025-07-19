17e33b0d58ad06a813cc0481cdbb8db9e904f1e0
Boom244 <bolumsba@gmail.com>
2025-07-19 12:13:50 -0700

    Added global navigational bar under global-components for use on all webpages

---
+++ src/about-us/aboutUs.js
@@ -0,0 +1,16 @@
+//Import navbar from global components
+import Navbar from '../components/Navbar';
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>About Us</h1>
+            <p>Welcome to our about us page! Here you can learn more about our mission, values, and the team behind our project.</p>
+            <p>We are committed to providing the best service and experience for our users. Our team is dedicated to continuous improvement and innovation.</p>
+        </div>
+    );
+};
+
+export default AboutUs; 
